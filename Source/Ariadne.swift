f0cbf4aee721f9ee2568413b536f18fa892b9f92
Denys Telezhkin <strangervir@gmail.com>
2019-02-07 17:54:21 +0200

    Add some small pieces of documentation.

--- Source/Ariadne.swift
+++ Source/Ariadne.swift
@@ -37,17 +37,26 @@ public protocol Routable {
     associatedtype Builder: ViewBuilder
 
     func perform(withViewFinder: ViewFinder,
-                context: Builder.Context,
-                completion: ((Bool) -> Void)?)
+                 context: Builder.Context,
+                 completion: ((Bool) -> Void)?)
 }
 
+/// Object responsible for performing navigation to concrete routes, as well as keeping references to root view provider and view finder.
 open class Router {
 
+    /// Object responsible for finding view on which route should be performed
     open var viewFinder: ViewFinder
+    
+    /// Object responsible for providing root view of interface hierarchy.
+    /// - Note: on iOS and tvOS, commonly, the root provider is UIWindow via UIApplication.shared.keyWindow, however there are scenarios where keyWindow might not be accessible, for example in iMesssage apps and application extensions. In those cases you can use root view controller that is accessible in those context, for example in iMessage extensions this could be `MSMessagesAppViewController`, or view controller presented on top of it.
+    /// Also, your app might have several UIWindow objects working in the same time, for example when app is using AirPlay, or if `UIWindow`s are used to present different interfaces modally. In those cases it's recommended to have multiple `Router` objects with different `RootViewProvider`s.
     open var rootViewProvider: RootViewProvider
 
     #if os(iOS) || os(tvOS)
 
+    /// Creates `Router` with `CurrentlyVisibleViewFinder` object set as a `ViewFinder` instance.
+    ///
+    /// - Parameter rootViewProvider: provider of the root view of interface
     public init(rootViewProvider: RootViewProvider) {
         self.viewFinder = CurrentlyVisibleViewFinder(rootViewProvider: rootViewProvider)
         self.rootViewProvider = rootViewProvider
@@ -55,6 +64,11 @@ open class Router {
 
     #else
 
+    /// Creates `Router` with specified root view provider and view finder.
+    ///
+    /// - Parameters:
+    ///   - rootViewProvider: provider of the root view of interface
+    ///   - viewFinder: object responsible for finding view on which route should be performed
     public init(rootViewProvider: RootViewProvider, viewFinder: ViewFinder) {
         self.viewFinder = viewFinder
         self.rootViewProvider = rootViewProvider
